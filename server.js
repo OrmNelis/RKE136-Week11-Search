0dd62870c08186451b37a4f4484c70b0fd9009ce
OrmNelis <orm.nelis@tktk.ee>
2023-12-13 14:39:23 +0200

    changes2

--- server.js
+++ server.js
@@ -7,21 +7,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/search', (req, res) => {
-    res.render('search', {movieData:''});
+    res.render('search', {movieDetails:''});
 });
 
 app.post('/search', (req, res) => {
     let movieTitle = req.body.movieTitle;
 
     let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=54550313e3c18136a8321c93223864f6&query=${movieTitle}`;
-    let genres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=54550313e3c18136a8321c93223864f6&language=en-US';
+    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=54550313e3c18136a8321c93223864f6&language=en-US';
    
-    let endpoints = [movieUrl, genres];
+    let endpoints = [movieUrl, genresUrl];
 
     axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
-    .then(
-        axios.spread((movie, genres) => {
-            const [general, generalComingSoon] = movie.data.results;
+    .then(axios.spread((movie, genres) => {
+            const [general] = movie.data.results;
             const movieGenreIds = general.genre_ids;        
             const movieGenres = genres.data.genres; 
             
@@ -29,7 +28,6 @@ app.post('/search', (req, res) => {
             for(let i = 0; i < movieGenreIds.length; i++) {
                 for(let j = 0; j < movieGenres.length; j++) {
                     if(movieGenreIds[i] === movieGenres[j].id) {
-                        console.log(movieGenres[j].name);
                         genresArray.push(movieGenres[j].name)
                     }
                     
@@ -43,7 +41,7 @@ app.post('/search', (req, res) => {
 
             genresToDisplay = genresToDisplay.slice(0, -2) + '.';
             
-            
+
             let movieObject = {
                 title: general.original_title,
                 year: new Date(general.release_date).getFullYear(),
@@ -53,12 +51,12 @@ app.post('/search', (req, res) => {
 
             };
 
-            res.render('search', {movieData: movieObject});
+            res.render('search', {movieDetails: movieObject});
         })
       );
     
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('server is running');
 });
